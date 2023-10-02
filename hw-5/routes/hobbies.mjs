b66a224d924d676dc9a4f07d9ca6f3c4f8203f4b
Inna Vashchuk <vashchuk.inna@gmail.com>
2023-10-01 17:38:03 -0700

    Add task 5

--- hw-5/routes/hobbies.mjs
+++ hw-5/routes/hobbies.mjs
@@ -1,63 +1,33 @@
-import http from 'http';
-import hobbiesData from '../data/hobbies.mjs';
-import usersData from '../data/users.mjs';
+import { getUserById } from './users.mjs';
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/hobbies' && req.method === 'GET') {
-        // Retrieve a list of hobbies
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(hobbiesData));
-    } else if (req.url.match(/\/users\/(\d+)\/hobbies/) && req.method === 'GET') {
-        // Retrieve a list of user hobbies
-        const userId = req.url.match(/\/users\/(\d+)\/hobbies/)[1];
-        const user = usersData.find(u => u.id === parseInt(userId));
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-            return;
-        }
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ hobbies: user.hobbies }));
-    } else if (req.url.match(/\/users\/(\d+)\/hobbies/) && req.method === 'POST') {
-        // Add a hobby for a specific user
-        const userId = req.url.match(/\/users\/(\d+)\/hobbies/)[1];
-        const user = usersData.find(u => u.id === parseInt(userId));
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-            return;
-        }
-
-        // Add a hobby to the user's hobbies array.
-        // const requestBody = await getRequestBody(req);
-        // user.hobbies.push(requestBody.hobby);
-
-        res.writeHead(201, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Hobby added successfully' }));
-    } else if (req.url.match(/\/users\/(\d+)\/hobbies/) && req.method === 'DELETE') {
-        // Delete a hobby for a specific user
-        const userId = req.url.match(/\/users\/(\d+)\/hobbies/)[1];
-        const user = usersData.find(u => u.id === parseInt(userId));
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-            return;
-        }
-
-        // Delete a hobby from the user's hobbies array.
-        const hobbyToDelete = req.url.split('/').pop();
-        const index = user.hobbies.indexOf(hobbyToDelete);
-        if (index !== -1) {
-            user.hobbies.splice(index, 1);
-        }
-
-        res.writeHead(204);
-        res.end();
+export function addHobby(userId, hobby) {
+  const user = getUserById(userId);
+  if (user) {
+    if (!user.hobbies.includes(hobby)) {
+      user.hobbies.push(hobby);
+      return true;
     } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Not Found' }));
+      return false;
     }
-});
+  }
+  return null;
+}
 
-export default server;
+export function deleteHobby(userId, hobby) {
+  const user = getUserById(userId);
+  if (user) {
+    const index = user.hobbies.indexOf(hobby);
+    if (index !== -1) {
+      user.hobbies.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return null;
+}
+
+export function getUserHobbies(userId) {
+  const user = getUserById(userId);
+  return user ? user.hobbies : null;
+}
--- hw-5/routes/users.mjs
+++ hw-5/routes/users.mjs
@@ -1,45 +1,53 @@
-import http from 'http';
-import usersData from '../data/users.mjs';
+import users from '../data/users.mjs';
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/users' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        const usersWithoutHobbies = usersData.map(user => {
-            const { hobbies, ...userWithoutHobbies } = user;
-            return userWithoutHobbies;
-        });
-        res.end(JSON.stringify(usersWithoutHobbies));
-    } else if (req.url.match(/\/users\/(\d+)/) && req.method === 'GET') {
-        const userId = req.url.match(/\/users\/(\d+)/)[1];
-        const user = usersData.find(u => u.id === parseInt(userId));
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-            return;
-        }
+function generateUserId() {
+  const ids = users.map(user => user.id);
+  const maxId = Math.max(...ids);
+  return maxId + 1;
+}
 
-        const userWithLinks = { ...user, links: [{ rel: 'hobbies', href: `/users/${userId}/hobbies` }] };
+export function createUser(user) {
+  const userId = generateUserId();
+  const newUser = { id: userId, ...user };
+  users.push(newUser);
+  return newUser;
+}
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(userWithLinks));
-    } else if (req.url.match(/\/users\/(\d+)/) && req.method === 'PATCH') {
-    } else if (req.url.match(/\/users\/(\d+)\/hobbies/) && req.method === 'GET') {
-        const userId = req.url.match(/\/users\/(\d+)\/hobbies/)[1];
-        const user = usersData.find(u => u.id === parseInt(userId));
-        if (!user) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-            return;
-        }
+export function deleteUser(userId) {
+  const index = users.findIndex(user => user.id === userId);
+  if (index !== -1) {
+    users.splice(index, 1);
+    return true; 
+  }
+  return false; 
+}
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ hobbies: user.hobbies }));
-    } else if (req.url === '/users' && req.method === 'POST') {
-    } else if (req.url.match(/\/users\/(\d+)/) && req.method === 'DELETE') {
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Not Found' }));
-    }
-});
+export function updateUser(userId, updatedData) {
+  const userIndex = users.findIndex(user => user.id === userId);
+  if (userIndex !== -1) {
+    users[userIndex] = { ...users[userIndex], ...updatedData };
+    return users[userIndex];
+  }
+  return null; 
+}
 
-export default server;
+export function getUserById(userId) {
+  return users.find(user => user.id === userId);
+}
+
+export function listUsers() {
+  const usersWithLinks = users.map(user => {
+    // Return user data with HATEOAS links
+    const { hobbies, ...userData } = user;
+    const userId = user.id;
+
+    userData.links = {
+      self: `/user?id=${userId}`,
+      hobbies: `/hobbies?id=${userId}`,
+    };
+
+    return userData;
+  });
+
+  return usersWithLinks;
+}
